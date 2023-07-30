4b6fc57eed94367bf73e8dc07937befe4be363f5
ItsMaSooD <140644625+ItsMaSooD@users.noreply.github.com>
2023-07-30 01:33:16 -0500

    Update DemoDay3.java

--- src/day3/DemoDay3.java
+++ src/day3/DemoDay3.java
@@ -5,5 +5,9 @@ public class DemoDay3 {
 	public static void main(String [] args) {
 		System.out.println("Masood");
 	}
+	public static void addInfo(){
+		String country = "USA";
+		String State = "VA";
+	}
 
 }
