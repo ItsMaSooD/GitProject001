1c9790cd84f122e395715dbc93a072d541ca8409
ItsMaSooD <140644625+ItsMaSooD@users.noreply.github.com>
2023-07-30 00:59:58 -0500

    Update DemoDay2.java

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -20,4 +20,9 @@ public class DemoDay2 {
 	public static void divid(int a, int b) {
 		System.out.println(a/b);
 	}
+	//next method --> divide()
+	public static void divid2(int a, int b) {
+		System.out.println(a/b);
+	}
+	
 }
